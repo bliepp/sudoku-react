515b3cf04a8ca9c8ab43152d330ec528f363b8bb
bliepp <12013202+bliepp@users.noreply.github.com>
2024-02-16 19:46:33 +0100

    Add navbar for settings, include bootstrap

--- src/App.js
+++ src/App.js
@@ -6,14 +6,41 @@ import Board from './Board.js';
 function App() {
   const [sideLength, setSideLength] = useState(9);
 
+  function chooseSize(size){
+    return function(e){
+      e.preventDefault();
+      setSideLength(size);
+    };
+  }
+
   return (
+    <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand">Sudoku Generator</span>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#/" onClick={(e) => e.preventDefault()}>Solve</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/" onClick={(e) => e.preventDefault()}>Reset</a>
+          </li>
+          <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#/" role="button" data-bs-toggle="dropdown" aria-expanded={false}>Size: { sideLength }x{sideLength}</a>
+              <ul className="dropdown-menu dropdown-menu-lg-end">
+                <li><h6 class="dropdown-header">Choose your size</h6></li>
+                <li><a className="dropdown-item" href="#/" onClick={chooseSize(4)}>4x4</a></li>
+                <li><a className="dropdown-item" href="#/" onClick={chooseSize(9)}>9x9</a></li>
+                <li><a className="dropdown-item" href="#/" onClick={chooseSize(16)}>16x16</a></li>
+              </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <div className="Board">
       <Board sideLength={sideLength} strokeWidth={40/sideLength} size={700} />
-      <br />
-      <button className="Sidelength" onClick={() => setSideLength(4)}>Set 4x4</button>
-      <button className="Sidelength" onClick={() => setSideLength(9)}>Set 9x9</button>
-      <button className="Sidelength" onClick={() => setSideLength(16)}>Set 16x16</button>
     </div>
+    </>
   );
 }
 
