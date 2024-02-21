cf96efcbe61fb27d57c53da8e6bfef9fa74d7c58
bliepp <12013202+bliepp@users.noreply.github.com>
2024-02-21 12:55:06 +0100

    Add actions menu, reload page when requesting same new sudoku type

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar({ exportRef }){
+    function forceRefresh(value){
+        const pathRegEx = /^\/(\d*x\d*)\/?/g;
+        const matches = [...useLocation().pathname.matchAll(pathRegEx)];
+        let isSudoku = matches.length > 0;
+
+        return function(event){
+            if (!isSudoku)
+                return
+
+            if (matches[0][1] === value){
+                event.preventDefault();
+                window.location.reload();
+            }
+        }
+    }
+
     return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-primary">
         <div className="container-fluid flex-lg-row-reverse">
@@ -14,13 +30,18 @@ export default function Navbar({ exportRef }){
                         <a className="nav-link" href="#/" role="button" data-bs-toggle="dropdown" aria-expanded={false}><i className="bi bi-plus-lg d-none d-lg-inline" /> New</a>
                         <ul className="dropdown-menu">
                             <li><h6 className="dropdown-header">Choose size</h6></li>
-                            <li><Link className="dropdown-item" to="/4x4/">4x4</Link></li>
-                            <li><Link className="dropdown-item" to="/9x9/">9x9</Link></li>
-                            <li><Link className="dropdown-item" to="/16x16/">16x16</Link></li>
+                            <li><Link className="dropdown-item" onClick={forceRefresh("4x4")} to="/4x4/">4x4</Link></li>
+                            <li><Link className="dropdown-item" onClick={forceRefresh("9x9")} to="/9x9/">9x9</Link></li>
+                            <li><Link className="dropdown-item" onClick={forceRefresh("16x16")} to="/16x16/">16x16</Link></li>
                         </ul>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#/" onClick={(e) => e.preventDefault()}>Solve</a>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link" href="#/" role="button" data-bs-toggle="dropdown" aria-expanded={false}>Actions</a>
+                        <ul className="dropdown-menu">
+                            <li><h6 className="dropdown-header">Choose action</h6></li>
+                            <li><a className="dropdown-item" href="#/">Solve</a></li>
+                            <li><a className="dropdown-item" href="#/">Restore</a></li>
+                        </ul>
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link" href="#/" role="button" data-bs-toggle="dropdown" aria-expanded={false}>Export</a>
