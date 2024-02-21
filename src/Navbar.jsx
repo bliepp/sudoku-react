7ec3950be8b4c0550f6768daeb3a95109a2356f5
bliepp <12013202+bliepp@users.noreply.github.com>
2024-02-21 22:42:02 +0100

    Bugfix: Close menu when going to root route

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Navbar({ exportRef, wavefunction }){
     return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-primary">
         <div className="container-fluid flex-lg-row-reverse">
-            <Link className="navbar-brand" to="/">Sudoku Generator</Link>
+            <Link className="navbar-brand" onClick={() => closeMobileMenu()} to="/">Sudoku Generator</Link>
             <button className="navbar-toggler" id="navbarCollapseBtn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
